cdfadab86250e6a7937c9bec20a1057ffe2d289f
Mathis Hermann <mathis.hermann@students.fhnw.ch>
2021-10-20 16:42:40 +0200

    wip

--- nginx_only.sh
+++ nginx_only.sh
@@ -1,9 +1,26 @@
 set -eo pipefail
 
+# Font Formatting for Output
+FONT_RESET="\x1B[0m"
+FONT_BOLD="\x1B[1m"
+FONT_DIM="\x1B[2m"
+FONT_UNDERLINE="\x1B[4m"
+FONT_WHITE="\x1B[97m"
+FONT_BG_RED="\x1B[41m"
+FONT_BG_GREEN="\x1B[42m"
+FONT_SUCCESS="${FONT_BG_GREEN}${FONT_WHITE}"
+FONT_ERROR="${FONT_BG_RED}${FONT_WHITE}"
+
+# Get Path and Name of the Script
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+SCRIPT_NAME=$(basename "${SCRIPT_PATH}")
+
+PHP_VER="8.0"
+
 main () 
 { 
     # 3rd party repositories are needed for specific versions of PHP
-    if [[ "8.0" != "" ]]; then
+    if [[ "${PHP_VER}" != "" ]]; then
         if hash "php" 2>/dev/null; then    
             PHP_VER_INSTALLED=sh php -v
             if [[ ${PHP_VER_INSTALLED} == "8.0"* ]]; then
@@ -33,7 +50,6 @@ main ()
 
     apt_install "git"
     apt_install "unzip"
-    apt_install "ufw"
     sudo curl -s https://getcomposer.org/installer | php
     sudo mv composer.phar /usr/local/bin/composer
 
@@ -53,14 +69,13 @@ main ()
 
     # Safety check to make sure that Apache is not already installed
     if hash apache2 2>/dev/null; then
-        apt remove --purge apache2
+        apt_remove "apache2"
         # >&2 echo -e "${FONT_ERROR}Error${FONT_RESET}, unable to install nginx because Apache is already setup on this server."
         # exit $ERR_GENERAL
     fi
 
     # Install nginx and PHP
     apt_install 'nginx'
-    ufw allow 'Nginx HTTP'    
 }
 
 apt_install ()
@@ -71,6 +86,17 @@ apt_install ()
         echo -e "Installing ${FONT_BOLD}${FONT_UNDERLINE}${1}${FONT_RESET}"
         apt install -y "$1"
         echo -e "${FONT_BOLD}${FONT_UNDERLINE}${1}${FONT_RESET} has been installed"
+    fi
+}
+
+apt_remove ()
+{
+    if hash "$1" 2>/dev/null; then
+        echo -e "Removing ${FONT_BOLD}${FONT_UNDERLINE}${1}${FONT_RESET}"
+        apt remove --purge -y "$1"
+        echo -e "${FONT_BOLD}${FONT_UNDERLINE}${1}${FONT_RESET} has been installed"
+    else
+        echo -e "${FONT_BOLD}${FONT_UNDERLINE}${1}${FONT_RESET} is not installed"
     fi
 }
 
