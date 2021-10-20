258edeeb3e1c497cf70dee1447d90b64d0377f85
Mathis Hermann <mathis.hermann@students.fhnw.ch>
2021-10-20 11:51:28 +0200

    wip

--- script.sh
+++ script.sh
@@ -377,7 +377,9 @@ check_apt ()
                 echo -e "PHP 8.0 already installed"
             fi
         else
+            echo -e "Updating apt"
             sudo apt update
+            echo -e "Installing dependencies"
             sudo apt install -y lsb-release ca-certificates apt-transport-https software-properties-common wget
             echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
             wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
