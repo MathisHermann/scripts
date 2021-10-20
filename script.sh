89c22d72605462d7e59584f11b17f0ac793622ef
Mathis Hermann <66256674+MathisHermann@users.noreply.github.com>
2021-10-20 10:28:06 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -557,7 +557,7 @@ get_user ()
     # if id -u ubuntu >/dev/null 2>&1; then
     #     printf 'ubuntu'
 
-    if [[ "$(uname -a)" == *"Debian"* ]]; then
+    if [[ "$(uname -a)" == *"1000"* ]]; then
        printf '%s' $(logname)
     else
         >&2 echo -e "${FONT_ERROR}Error${FONT_RESET}, This script currently only runs on Ubuntu."
